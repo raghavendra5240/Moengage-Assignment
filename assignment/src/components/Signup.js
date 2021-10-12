c7ef264f1a92ebff4ee9dc2f424e82670c91e5b5
raghavendra5240 <raghavendra5240@gmail.com>
2021-10-12 14:01:07 +0530

    assignmnet

--- assignment/src/components/Signup.js
+++ assignment/src/components/Signup.js
@@ -1,173 +1,132 @@
-import {useState,useRef } from "react";
-import React from 'react';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import {Link} from "react-router-dom"
-import { useHistory } from "react-router-dom";
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-    padding:90,
-    display:'flex',
-    flexDirection:"column"
-  },
-  paper: {
-    height:430,
-    width:370,
-    backgroundColor:"white",
-   padding:40,
-  display:"block",
-  marginLeft:"auto",
-  marginRight:"auto"
-  },
- 
-  heading :{
-    color:'#002884',
-     display:"block",
-  marginLeft:60,
-  marginRight:"auto",
-   fontSize:30,
-    fontWeight:"bold"
-
-  },
-  subheading:{
-    color:'#002884',
-   textAlign:"left",
-    fontSize:15,
- 
-  },
- 
-  image:{
-    height:100,
-    width:100},
-  div1 :{
-    display:"flex",
-    flexDirection:"row"
-  } ,
-  upload :{
-     display:"flex"
-  } ,
-  link:{
-    textDecoration:"none",
-    color:"white"
-  },
-  div2:{
-    marginLeft:100
-  },
-  button:{
-    width:230
-  }
-
-}));
-
-
-export default function Signin() {
- const classes = useStyles();
-
-   
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [signup,setSignup]=useState([])
-
-  
-
-  const onsubmit = (e) => {
-    e.preventDefault();
-    setSignup([
-      ...signup,
-
-      {
-        id: new Date().getTime().toString(),
-        name:name,
-        email: email,
-        password:password, 
-      },
-    ]);
-    
-  };
-   
-  const history = useHistory();
-
-  const routeChange = () =>{ 
-    let path = `/login`; 
-    history.push(path);
-  }
-
-
- 
+function Copyright(props) {
   return (
-    
-     <>
-      <div className={classes.root}>
-        <form onSubmit={(e) => {
-          onsubmit(e);
-        }} >
-       
-        
-        <br/><br/>
-         <div className={classes.div1}>
-        <Paper className={classes.paper}>
-          <div className={classes.div2}>
-        <Typography className={classes.heading}>  Sign Up</Typography>
-        <br /><br />
-        <Typography className={classes.subheading}> Enter Name </Typography>
-        
-        <TextField
-        variant="outlined"
-          type="text"
-          placeholder="Enter name"
-          value={name}
-          onChange={(e) => {
-            setName(e.target.value);
-          }}
-          required
-        />
-         <br /><br />
-         <Typography className={classes.subheading}> Enter mail id </Typography>
-        
-        <TextField
-        variant="outlined"
-          type="text"
-          placeholder="Enter mail id "
-          value={email}
-          onChange={(e) => {
-            setEmail(e.target.value);
-          }}
-          required
-        />
-        <br />
-        
-       
-        <br />
-        <Typography className={classes.subheading}> Enter Password </Typography>
-        <TextField
-        variant="outlined"
-          type="text"
-           placeholder="Enter Password "
-          value={password}
-          onChange={(e) => {
-            setPassword(e.target.value);
-          }}
-          required
-        />
-        <br /><br />
-       
-         <Button variant="contained" color="primary" type="submit" className={classes.button}  onClick={routeChange}> Sign in</Button><br/><br/>
-        <Link to="/login">
-               Already have an account? Sign In
-              </Link>
-              </div>
-          </Paper>  
-        </div>
-          </form> 
-          
-    </div>
-   </>
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
   );
 }
 
+const theme = createTheme();
+
+export default function SignUp() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    // eslint-disable-next-line no-console
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  autoComplete="given-name"
+                  name="firstName"
+                  required
+                  fullWidth
+                  id="firstName"
+                  label="First Name"
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  id="lastName"
+                  label="Last Name"
+                  name="lastName"
+                  autoComplete="family-name"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <FormControlLabel
+                  control={<Checkbox value="allowExtraEmails" color="primary" />}
+                  label="Please check the Checkbox"
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign Up
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link href="#" variant="body2">
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 5 }} />
+      </Container>
+    </ThemeProvider>
+  );
+}
